466ffa214b785ff1e7c9663decd562a697286533
Mallikarjuna Reddy Gali <m.gali@dxc.com>
2023-03-03 20:07:32 +0530

    second commit

---
+++ src/main/java/com/example/demo/config/DummyClassConfig.java
@@ -0,0 +1,8 @@
+package com.example.demo.config;
+
+public class DummyClassConfig {
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/main/java/com/example/demo/config/EhCacheConfiguration.java
+++ src/main/java/com/example/demo/config/EhCacheConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
