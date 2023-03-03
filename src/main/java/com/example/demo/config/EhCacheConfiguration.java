6f1db75461c52ca579a3e155395b27da135d3213
Mallikarjuna Reddy Gali <m.gali@dxc.com>
2023-03-03 20:01:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/config/EhCacheConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.demo.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EhCacheConfiguration {
+
+
+}
